773e9fdb97a18df2cb8689611b79f19207d080f5
wenji <verda.zhao@hcwins.com>
2015-04-21 10:30:18 +0800

    modify a error

--- evs/src/test/java/com/hcwins/vehicle/ta/evs/apiobj/enterprise/CaptchaRegistRequest.java
+++ evs/src/test/java/com/hcwins/vehicle/ta/evs/apiobj/enterprise/CaptchaRegistRequest.java
@@ -7,7 +7,7 @@ import com.hcwins.vehicle.ta.evs.apiobj.BaseRequest;
  * Created by tommy on 3/24/15.
  */
 public class CaptchaRegistRequest extends BaseRequest {
-    protected final static String api = EVSUtil.getAPISet().enterprise.captchaRegist;
+    protected final static String api = EVSUtil.getAPISet().enterprise.CaptchaRegist;
 
     public String mobile;
 
